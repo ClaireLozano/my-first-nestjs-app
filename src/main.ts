3ecb17c1b12678995d7a7ffb5586256a0a8d7986
Claire Lozano <claire.lozano@live.fr>
2024-05-28 11:31:38 +0200

    allow localhost:4200

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	app.use(
+		cors({
+			origin: 'http://localhost:4200',
+			methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+			credentials: true,
+			allowedHeaders: 'Content-Type, Authorization',
+		}),
+	);
+
 	await app.listen(3000);
 }
 bootstrap();
